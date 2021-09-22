e184bbeffddd0e9d0a727809ea2241cb2c6406e7
Sergery Rutkousky <dr.bibelman@gmail.com>
2021-09-22 19:19:14 +0300

    post css sorting

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,8 @@ const gulp = require('gulp');
 const pug = require('gulp-pug');
 const sass = require('gulp-sass')(require('sass'));
 const clean = require('gulp-clean');
+const postcss = require('gulp-postcss');
+const sorting = require('postcss-sorting');
 
 exports.views = () => {
     return src('./src/*.pug')
@@ -15,6 +17,180 @@ exports.views = () => {
         )
         .pipe(dest('./dist'));
 };
+
+
+
+exports.sorting = () => {
+    return  gulp.src('./dist/css/*.css').pipe(
+        postcss([
+            sorting({ 
+                "order": [
+                    "custom-properties",
+                    "dollar-variables",
+                    {
+                      "type" : "at-rule",
+                      "name" : "include",
+                    },
+                    {
+                      "type"     : "at-rule",
+                      "name"     : "include",
+                      "hasBlock" : true
+                    },
+                    "declarations",
+                    {
+                      "type"     : "rule",
+                      "selector" : /^&/
+                    },
+                    {
+                      "type"     : "rule",
+                      "selector" : /^&:\w+$/
+                    },
+                    {
+                      "type"     : "rule",
+                      "selector" : /^&:   : \w+$/
+                    },
+                    {
+                      "type" : "at-rule",
+                      "name" : "media"
+                    }
+                  ],
+                  "properties-order": [
+                    "order",
+                    "z-index",
+                    "position",
+                    "top",
+                    "bottom",
+                    "left",
+                    "right",
+                    "content",
+                    "visibility",
+                    "backface-visibility",
+                    "opacity",
+                    "filter",
+                    "mix-blend-mode",
+                    "transform",
+                    "transform-style",
+                    "perspective",
+                    "display",
+                    "flex-direction",
+                    "flex-flow",
+                    "justify-content",
+                    "align-items",
+                    "align-content",
+                    "flex-basis",
+                    "flex-wrap",
+                    "flex-grow",
+                    "flex-shrink",
+                    "grid",
+                    "grid-template",
+                    "grid-template-columns",
+                    "grid-auto-columns",
+                    "grid-template-rows",
+                    "grid-auto-rows",
+                    "grid-template-areas",
+                    "grid-auto-flow",
+                    "grid-gap",
+                    "outline",
+                    "outline-width",
+                    "outline-style",
+                    "outline-color",
+                    "outline-offset",
+                    "border",
+                    "border-top",
+                    "border-right",
+                    "border-bottom",
+                    "border-left",
+                    "border-radius",
+                    "border-collapse",
+                    "margin",
+                    "margin-top",
+                    "margin-right",
+                    "margin-bottom",
+                    "margin-left",
+                    "background",
+                    "background-image",
+                    "background-color",
+                    "background-position",
+                    "background-size",
+                    "background-repeat",
+                    "background-origin",
+                    "background-clip",
+                    "background-attachment",
+                    "max-height",
+                    "min-height",
+                    "height",
+                    "max-width",
+                    "min-width",
+                    "width",
+                    "resize",
+                    "box-sizing",
+                    "overflow",
+                    "overflow-x",
+                    "overflow-y",
+                    "scroll-behavior",
+                    "object-fit",
+                    "object-position",
+                    "float",
+                    "padding",
+                    "padding-top",
+                    "padding-right",
+                    "padding-bottom",
+                    "padding-left",
+                    "empty-cells",
+                    "list-style",
+                    "list-style-image",
+                    "list-style-type",
+                    "list-style-position",
+                    "text-indent",
+                    "word-break",
+                    "word-wrap",
+                    "word-spacing",
+                    "text-overflow",
+                    "hyphens",
+                    "white-space",
+                    "text-decoration",
+                    "vertical-align",
+                    "text-transform",
+                    "letter-spacing",
+                    "direction",
+                    "text-shadow",
+                    "font",
+                    "font-style",
+                    "font-family",
+                    "font-size",
+                    "line-height",
+                    "font-weight",
+                    "font-stretch",
+                    "font-kerning",
+                    "font-variant",
+                    "color",
+                    "cursor",
+                    "pointer-events",
+                    "transition",
+                    "transition-property",
+                    "transition-duration",
+                    "transition-timing-function",
+                    "transition-delay",
+                    "animation",
+                    "animation-name",
+                    "animation-duration",
+                    "animation-timing-function",
+                    "animation-delay",
+                    "animation-iteration-count",
+                    "animation-direction",
+                    "animation-fill-mode",
+                    "animation-play-state"
+                  ],
+                  "unspecified-properties-position" : "bottom"
+             })
+        ])
+    ).pipe(
+        gulp.dest('./css/src')
+    );
+};
+
+
+
 
 exports.img = () => {
     return gulp.src('./src/images/**/*.*')
