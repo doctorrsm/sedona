ea0c5de67e30bbafda27c78807d3377e2f129c6f
Sergery Rutkousky <dr.bibelman@gmail.com>
2021-09-24 01:02:28 +0300

    Сделал browser sync Для html.

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,12 @@
 const { src, dest } = require('gulp');
-const { watch, series } = require('gulp');
+const { watch, series, parallel } = require('gulp');
 const gulp = require('gulp');
 const pug = require('gulp-pug');
 const sass = require('gulp-sass')(require('sass'));
 const clean = require('gulp-clean');
 const postcss = require('gulp-postcss');
 const sorting = require('postcss-sorting');
+const browserSync = require('browser-sync').create();
 
 exports.views = () => {
     return src('./src/*.pug')
@@ -197,6 +198,31 @@ exports.img = () => {
     .pipe(gulp.dest('./dist/images'));
 };
 
+exports.browsersyncServe = () =>{
+  browserSync.init({
+    server: {
+      baseDir: "dist"
+  }
+  });
+ 
+}
+
+function browsersyncServe(){
+  browserSync.init({
+    server: {
+      baseDir: "dist"
+  }
+  });
+ 
+}
+
+function browsersyncReload(){
+  browserSync.reload();
+  
+}
+
+
+
 function html() {
     return src('./src/*.pug')
     .pipe(
@@ -220,11 +246,10 @@ function css() {
     .pipe(gulp.dest('./dist/css'));
 };
 
+function watchTask() {
+   watch('./src/**/**/*.pug').on( 'change', series(html, browsersyncReload));
+    watch('./src/sass/**/*.scss', series(css,browsersyncReload));
+}
 
-exports.default = function() {
-    gulp.series(html,css,img);
-    //watch('./src/*.pug', series(html));
-    watch('./src/**/**/*.pug', series(html));
-    watch('./src/sass/**/*.scss', series(css));
-    
-};
+exports.default = 
+    parallel(html,css,img, parallel(watchTask,browsersyncServe));
